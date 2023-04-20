5c6926d9302ef5156793666d91e14978ed1dce9f
lsh80165 <lsh80165@gmail.com>
2023-04-20 09:02:09 +0900

    Feat : 스프링 시큐리티 설정

---
+++ backend/src/main/java/com/ssafy/backend/global/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.ssafy.backend.global.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+
+        http.oauth2Login()
+                .authorizationEndpoint();
+
+        return http.build();
+    }
+}
