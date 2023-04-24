917da23e3c55c820f602fff1496cff3742e009e3
이성복 <seungbok3240@gmail.com>
2023-04-24 13:43:32 +0900

    Feat: 깃허브 크롤링과 스프링 API 연동

--- backend/src/main/java/com/ssafy/backend/domain/github/controller/GithubController.java
+++ backend/src/main/java/com/ssafy/backend/domain/github/controller/GithubController.java
@@ -1,6 +1,8 @@
 package com.ssafy.backend.domain.github.controller;
 
 import com.ssafy.backend.domain.github.service.GithubCrawlingService;
+import com.ssafy.backend.global.response.CommonResponse;
+import com.ssafy.backend.global.response.ResponseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,8 +13,11 @@ public class GithubController {
 
     private final GithubCrawlingService crawlingService;
 
-    @PatchMapping("/crawling/{nickname}")
-    public void githubCrawling(@PathVariable String nickname) {
-        crawlingService.get(nickname, 1L);
+    private final ResponseService responseService;
+
+    @PatchMapping("/crawling/{nickname}/{userId}")
+    public CommonResponse githubCrawling(@PathVariable String nickname, @PathVariable long userId) {
+        crawlingService.getGithubInfo(nickname, userId);
+        return responseService.getSuccessResponse();
     }
 }
