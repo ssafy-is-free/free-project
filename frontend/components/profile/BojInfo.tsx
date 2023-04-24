4132a530f3f3246994c623fa4f0f8f9d3ae8671f
happyd918 <0happydeath0@naver.com>
2023-04-24 15:50:14 +0900

    Feat : 프로필 깃헙 백준 정보

---
+++ frontend/components/profile/BojInfo.tsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import { IBojInfo } from './IProfile';
+import CircleChart from './CircleChart';
+
+const BojInfoDiv = styled.div``;
+const BasicInfoDiv = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  grid-template-rows: auto auto;
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+`;
+
+const BoxDiv = styled.div`
+  border-radius: 1rem;
+  background-color: ${(props) => props.theme.secondary};
+  padding: 1.3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  img {
+    display: block;
+    margin-left: auto;
+  }
+`;
+
+export default function BojInfo({ boj }: IBojInfo) {
+  const infoList = [
+    {
+      name: '맞은 문제',
+      value: boj.pass,
+      icon: 'Icon/CorrectIcon.svg',
+    },
+    {
+      name: '틀렸습니다',
+      value: boj.fail,
+      icon: 'Icon/WrongIcon.svg',
+    },
+    {
+      name: '제출',
+      value: boj.submit,
+      icon: 'Icon/SubmitIcon.svg',
+    },
+    {
+      name: '시도했지만 맞지 못한 문제',
+      value: boj.tryFail,
+      icon: 'Icon/CryIcon.svg',
+    },
+  ];
+
+  return (
+    <BojInfoDiv>
+      <BasicInfoDiv>
+        {infoList.map((info, idx) => (
+          <BoxDiv key={idx}>
+            <div>
+              <h4>{info.name}</h4>
+              <p>{info.value}</p>
+            </div>
+            <img src={info.icon} alt="IC" />
+          </BoxDiv>
+        ))}
+      </BasicInfoDiv>
+      <CircleChart data={boj.languages} label={true}></CircleChart>
+    </BojInfoDiv>
+  );
+}
---
+++ frontend/components/profile/GithubInfo.tsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components';
+import { IGithubInfo } from './IProfile';
+import CircleChart from './CircleChart';
+
+const GithubDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const CommitDiv = styled.div`
+  display: flex;
+
+  div {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const LanguageDiv = styled.div``;
+
+const RepoDiv = styled.div`
+  padding-bottom: 1rem;
+  p {
+    margin-bottom: 1rem;
+  }
+`;
+
+const RepoItemDiv = styled.div`
+  border: 1px solid;
+`;
+
+export default function GithubInfo({ github }: IGithubInfo) {
+  const basicInfo = [
+    {
+      name: 'Commits',
+      data: github.commit,
+    },
+    {
+      name: 'Star',
+      data: github.star,
+    },
+    {
+      name: 'Followers',
+      data: github.followers,
+    },
+  ];
+
+  return (
+    <GithubDiv>
+      <CommitDiv>
+        {basicInfo.map((info, idx) => (
+          <div key={idx}>
+            <h4>{info.data}</h4>
+            <p>{info.name}</p>
+          </div>
+        ))}
+      </CommitDiv>
+      <LanguageDiv>
+        <p>Language</p>
+        <CircleChart data={github.languages} label={true}></CircleChart>
+      </LanguageDiv>
+      <RepoDiv>
+        <p>Repositiories</p>
+        <div>
+          {github.repositories.map((repo, idx) => (
+            <RepoItemDiv key={idx}>{repo.name}</RepoItemDiv>
+          ))}
+        </div>
+      </RepoDiv>
+    </GithubDiv>
+  );
+}
