eba386e2fc8d715f31b15de565933a28abab1762
noobsoda <20151565@vision.hoseo.edu>
2023-04-25 13:14:07 +0900

    Fix : QClass 버그 긴급 수정

--- backend/src/main/java/com/ssafy/backend/domain/algorithm/repository/BojRepositorySupport.java
+++ backend/src/main/java/com/ssafy/backend/domain/algorithm/repository/BojRepositorySupport.java
@@ -1,30 +1,18 @@
 package com.ssafy.backend.domain.algorithm.repository;
 
-import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.NumberExpression;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssafy.backend.domain.algorithm.dto.response.BojMyRankResponseDTO;
-import com.ssafy.backend.domain.algorithm.dto.response.QBojMyRankResponseDTO;
-import com.ssafy.backend.domain.entity.Baekjoon;
-import com.ssafy.backend.domain.entity.QBaekjoon;
-import com.ssafy.backend.domain.entity.QUser;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-import static com.ssafy.backend.domain.entity.QBaekjoon.baekjoon;
-import static com.ssafy.backend.domain.entity.QUser.user;
-
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.backend.domain.entity.Baekjoon;
 
 @Repository
 public class BojRepositorySupport extends QuerydslRepositorySupport {
-    private final JPAQueryFactory queryFactory;
-    public BojRepositorySupport(JPAQueryFactory queryFactory) {
-        super(Baekjoon.class);
-        this.queryFactory = queryFactory;
-    }
+	private final JPAQueryFactory queryFactory;
+
+	public BojRepositorySupport(JPAQueryFactory queryFactory) {
+		super(Baekjoon.class);
+		this.queryFactory = queryFactory;
+	}
 
 }
