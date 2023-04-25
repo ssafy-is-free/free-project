9710254b5c98fb5894c9fa5ab69fc361e578d2f2
yeonjin <y_jin99@naver.com>
2023-04-25 11:01:29 +0900

    Fix : rank 정보 추가

--- backend/src/main/java/com/ssafy/backend/domain/entity/Baekjoon.java
+++ backend/src/main/java/com/ssafy/backend/domain/entity/Baekjoon.java
@@ -1,15 +1,24 @@
 package com.ssafy.backend.domain.entity;
 
+import static javax.persistence.GenerationType.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+
 import com.ssafy.backend.domain.algorithm.dto.response.BojInformationResponseDTO;
 import com.ssafy.backend.domain.entity.common.BaseTimeEntity;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.DynamicUpdate;
-
-import javax.persistence.*;
-
-import static javax.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Entity
@@ -18,55 +27,60 @@ import static javax.persistence.GenerationType.IDENTITY;
 @Table(name = "baekjoon")
 public class Baekjoon extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "id")
-    private long id;
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "id")
+	private long id;
 
-    @Column(name = "tier", nullable = false)
-    private String tier;
+	@Column(name = "tier", nullable = false)
+	private String tier;
 
-    @Column(name = "pass_count", nullable = false)
-    private int passCount;
+	@Column(name = "pass_count", nullable = false)
+	private int passCount;
 
-    @Column(name = "try_fail_count", nullable = false)
-    private int tryFailCount;
+	@Column(name = "try_fail_count", nullable = false)
+	private int tryFailCount;
 
-    @Column(name = "submit_count", nullable = false)
-    private int submitCount;
+	@Column(name = "submit_count", nullable = false)
+	private int submitCount;
 
-    @Column(name = "fail_count", nullable = false)
-    private int failCount;
+	@Column(name = "fail_count", nullable = false)
+	private int failCount;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false, unique = true)
-    private User user;
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false, unique = true)
+	private User user;
 
-    @Builder
-    public Baekjoon(long id, String tier, int passCount, int tryFailCount, int submitCount, int failCount, User user) {
-        this.id = id;
-        this.tier = tier;
-        this.passCount = passCount;
-        this.tryFailCount = tryFailCount;
-        this.submitCount = submitCount;
-        this.failCount = failCount;
-        this.user = user;
-    }
-    public static Baekjoon createBaekjoon(BojInformationResponseDTO bojInformationResponseDTO, User user){
-        return Baekjoon.builder()
-                .tier(bojInformationResponseDTO.getTier())
-                .passCount(bojInformationResponseDTO.getPassCount())
-                .tryFailCount(bojInformationResponseDTO.getTryFailCount())
-                .submitCount(bojInformationResponseDTO.getSubmitCount())
-                .failCount(bojInformationResponseDTO.getFailCount())
-                .user(user)
-                .build();
-    }
-    public void updateBaekjoon(BojInformationResponseDTO bojInformationResponseDTO){
-        this.tier = bojInformationResponseDTO.getTier();
-        this.passCount = bojInformationResponseDTO.getPassCount();
-        this.tryFailCount = bojInformationResponseDTO.getTryFailCount();
-        this.submitCount = bojInformationResponseDTO.getSubmitCount();
-        this.failCount = bojInformationResponseDTO.getFailCount();
-    }
+	@Column(name = "boj_previous_rank", nullable = false)
+	private long previousRank;
+
+	@Builder
+	public Baekjoon(long id, String tier, int passCount, int tryFailCount, int submitCount, int failCount, User user) {
+		this.id = id;
+		this.tier = tier;
+		this.passCount = passCount;
+		this.tryFailCount = tryFailCount;
+		this.submitCount = submitCount;
+		this.failCount = failCount;
+		this.user = user;
+	}
+
+	public static Baekjoon createBaekjoon(BojInformationResponseDTO bojInformationResponseDTO, User user) {
+		return Baekjoon.builder()
+			.tier(bojInformationResponseDTO.getTier())
+			.passCount(bojInformationResponseDTO.getPassCount())
+			.tryFailCount(bojInformationResponseDTO.getTryFailCount())
+			.submitCount(bojInformationResponseDTO.getSubmitCount())
+			.failCount(bojInformationResponseDTO.getFailCount())
+			.user(user)
+			.build();
+	}
+
+	public void updateBaekjoon(BojInformationResponseDTO bojInformationResponseDTO) {
+		this.tier = bojInformationResponseDTO.getTier();
+		this.passCount = bojInformationResponseDTO.getPassCount();
+		this.tryFailCount = bojInformationResponseDTO.getTryFailCount();
+		this.submitCount = bojInformationResponseDTO.getSubmitCount();
+		this.failCount = bojInformationResponseDTO.getFailCount();
+	}
 }
