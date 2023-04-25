16c55000eb402925c25ab6cd18d6f462f7621ca3
noobsoda <20151565@vision.hoseo.edu>
2023-04-25 13:24:32 +0900

    Fix : User boj patch로 수정 안쓰는 코드 주석

--- backend/src/main/java/com/ssafy/backend/domain/algorithm/controller/AlgorithmController.java
+++ backend/src/main/java/com/ssafy/backend/domain/algorithm/controller/AlgorithmController.java
@@ -4,15 +4,12 @@ import static com.ssafy.backend.global.response.CustomSuccessStatus.*;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.backend.domain.algorithm.dto.response.BojMyRankResponseDTO;
 import com.ssafy.backend.domain.algorithm.service.AlgorithmService;
 import com.ssafy.backend.global.auth.dto.UserPrincipal;
-import com.ssafy.backend.global.response.CommonResponse;
 import com.ssafy.backend.global.response.DataResponse;
 import com.ssafy.backend.global.response.ResponseService;
 
@@ -38,12 +35,12 @@ public class AlgorithmController {
 		return bojMyRankResponseDTO == null ? responseService.getDataResponse(null, RESPONSE_NO_CONTENT) :
 			responseService.getDataResponse(bojMyRankResponseDTO, RESPONSE_SUCCESS);
 	}
-
-	@PatchMapping("")
+	// TODO: 2023-04-25 12시에 한꺼번에 배치할떄 사용할 백준 크롤링 
+	/*@PatchMapping("")
 	public CommonResponse bojSaveUser(@RequestParam Long userId) {
 
 		algorithmService.patchBojByUserId(userId);
 		return responseService.getSuccessResponse();
-	}
+	}*/
 
 }
--- backend/src/main/java/com/ssafy/backend/domain/user/controller/BojController.java
+++ backend/src/main/java/com/ssafy/backend/domain/user/controller/BojController.java
@@ -1,15 +1,21 @@
 package com.ssafy.backend.domain.user.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ssafy.backend.domain.user.dto.BojIdRequestDTO;
 import com.ssafy.backend.domain.user.service.BojService;
 import com.ssafy.backend.global.auth.dto.UserPrincipal;
 import com.ssafy.backend.global.response.CommonResponse;
 import com.ssafy.backend.global.response.ResponseService;
-import lombok.Getter;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,28 +23,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 public class BojController {
 
-    private final BojService bojService;
-    private final ResponseService responseService;
+	private final BojService bojService;
+	private final ResponseService responseService;
 
+	@PatchMapping("boj-id")
+	public CommonResponse saveBojId(
+		@RequestBody BojIdRequestDTO bojIdRequestDTO,
+		@AuthenticationPrincipal UserPrincipal userPrincipal) {
 
-    @PostMapping("boj-id")
-    public CommonResponse saveBojId(
-            @RequestBody BojIdRequestDTO bojIdRequestDTO,
-            @AuthenticationPrincipal UserPrincipal userPrincipal){
+		long userId = userPrincipal.getId();
 
-        long userId = userPrincipal.getId();
+		bojService.saveId(userId, bojIdRequestDTO);
 
-        bojService.saveId(userId,bojIdRequestDTO);
+		return responseService.getSuccessResponse();
+	}
 
-        return responseService.getSuccessResponse();
-    }
+	@GetMapping("boj-id")
+	public CommonResponse checkBojId(
+		@RequestParam("id") String bojId) {
 
-    @GetMapping("boj-id")
-    public CommonResponse checkBojId(
-            @RequestParam("id") String bojId){
+		bojService.checkDuplicateId(bojId);
 
-        bojService.checkDuplicateId(bojId);
-
-        return responseService.getSuccessResponse();
-    }
+		return responseService.getSuccessResponse();
+	}
 }
