42594e1758a3bdcd3175c090413a7fcda7304521
lsh80165 <lsh80165@gmail.com>
2023-04-26 13:07:58 +0900

    Fix : 젠킨스 파일 수정 - 컴포즈 down시에 컨테이너 올라와있는지 확인

--- backend/src/main/java/com/ssafy/backend/global/auth/exception/TokenAuthenticationEntryPoint.java
+++ backend/src/main/java/com/ssafy/backend/global/auth/exception/TokenAuthenticationEntryPoint.java
@@ -1,24 +1,27 @@
 package com.ssafy.backend.global.auth.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.backend.global.response.CommonResponse;
-import com.ssafy.backend.global.response.ResponseService;
-import com.ssafy.backend.global.response.exception.CustomExceptionStatus;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
+import static com.ssafy.backend.global.auth.filter.TokenAuthenticationFilter.*;
+import static com.ssafy.backend.global.response.exception.CustomExceptionStatus.TOKEN_EXPIRE;
+import static com.ssafy.backend.global.response.exception.CustomExceptionStatus.TOKEN_ILLEGAL;
+import static com.ssafy.backend.global.response.exception.CustomExceptionStatus.TOKEN_INVALID;
+import static com.ssafy.backend.global.response.exception.CustomExceptionStatus.TOKEN_UNSUPPORTED;
+
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-import static com.ssafy.backend.global.auth.filter.TokenAuthenticationFilter.TOKEN_EXCEPTION_KEY;
-import static com.ssafy.backend.global.response.exception.CustomExceptionStatus.*;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.backend.global.response.CommonResponse;
+import com.ssafy.backend.global.response.ResponseService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 // TODO: 2023-04-23 json 형태를 따라서 error 일때도 별도로 메시지를 만들어서 보내줘야됨
 // TODO: 2023-04-23 팀원들과 상의해서 표준예외로 갈것인지 논의 필요
@@ -32,34 +35,33 @@ public class TokenAuthenticationEntryPoint implements AuthenticationEntryPoint {
     private final ObjectMapper objectMapper;
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+        AuthenticationException authException) throws IOException, ServletException {
 
         log.error("Authentication error");
 
-        String exception = (String) request.getAttribute(TOKEN_EXCEPTION_KEY);
+        String exception = (String)request.getAttribute(TOKEN_EXCEPTION_KEY);
 
-        if(exception == null) return;
+        if (exception == null)
+            return;
 
         CommonResponse exceptionResponse;
 
-        if(exception.equals(TOKEN_INVALID)){
+        if (exception.equals(TOKEN_INVALID)) {
             exceptionResponse = responseService.getExceptionResponse(TOKEN_INVALID);
-        }
-        else if(exception.equals(TOKEN_EXPIRE)){
+        } else if (exception.equals(TOKEN_EXPIRE)) {
             exceptionResponse = responseService.getExceptionResponse(TOKEN_EXPIRE);
-        }
-        else if(exception.equals(TOKEN_UNSUPPORTED)){
+        } else if (exception.equals(TOKEN_UNSUPPORTED)) {
             exceptionResponse = responseService.getExceptionResponse(TOKEN_UNSUPPORTED);
-        }
-        else{
+        } else {
             exceptionResponse = responseService.getExceptionResponse(TOKEN_ILLEGAL);
         }
-
 
         String error = objectMapper.writeValueAsString(exceptionResponse);
 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
+
         response.setStatus(401);
 
         response.getWriter().write(error);
