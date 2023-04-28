649e95356fb851c42deb736ea31d6e1414c277fe
noobsoda <20151565@vision.hoseo.edu>
2023-04-28 14:23:11 +0900

    Fix : Test 세팅 수정

--- backend/src/test/java/com/ssafy/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/ssafy/backend/BackendApplicationTests.java
@@ -1,13 +1,12 @@
 package com.ssafy.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
+/*	@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
