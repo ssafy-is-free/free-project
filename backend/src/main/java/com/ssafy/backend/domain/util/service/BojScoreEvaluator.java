7567841f3657f29f3bff8ea55165231b5b28c92c
noobsoda <20151565@vision.hoseo.edu>
2023-04-28 10:49:48 +0900

    Feat : 백준 스코어 로직 변경

--- backend/src/main/java/com/ssafy/backend/domain/util/service/BojScoreEvaluator.java
+++ backend/src/main/java/com/ssafy/backend/domain/util/service/BojScoreEvaluator.java
@@ -8,10 +8,13 @@ import com.ssafy.backend.domain.entity.Baekjoon;
 public class BojScoreEvaluator {
 	public static int scoreEvaluator(Baekjoon baekjoon) {
 		//제출문제 - (틀린문제 / 맞은문제) * 제출 - 시도했지만 맞은 문제 * (티어 / 맞은문제)
-		double score = Math.ceil(((double)baekjoon.getFailCount() / baekjoon.getPassCount()) * baekjoon.getSubmitCount()
-			- baekjoon.getTryFailCount() * (getTierNumberFromUrl(baekjoon.getTier()) / (double)baekjoon.getPassCount())
-			* 0.5);
-		return (int)score;
+		int score = (int)Math.ceil(
+			((double)baekjoon.getFailCount() / baekjoon.getPassCount()) * baekjoon.getSubmitCount()
+				- baekjoon.getTryFailCount() * (getTierNumberFromUrl(baekjoon.getTier())
+				/ (double)baekjoon.getPassCount())
+				* 0.5);
+
+		return Math.max(score, 0);
 	}
 
 	// 티어에서 숫자만 파싱하는 함수
@@ -19,10 +22,6 @@ public class BojScoreEvaluator {
 		Pattern pattern = Pattern.compile("tier/(\\d+)");
 		Matcher matcher = pattern.matcher(tier);
 
-		if (matcher.find()) {
-			return Integer.parseInt(matcher.group(1));
-		} else {
-			return 0;
-		}
+		return (matcher.find()) ? Integer.parseInt(matcher.group(1)) : 0;
 	}
 }
