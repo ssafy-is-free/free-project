06ded330b46b8a65b7a42e5fcb03862eb6fab856
noobsoda <20151565@vision.hoseo.edu>
2023-04-28 10:39:50 +0900

    Feat : 백준 스코어 로직 추가

--- backend/src/main/java/com/ssafy/backend/domain/util/service/BojScoreEvaluator.java
+++ backend/src/main/java/com/ssafy/backend/domain/util/service/BojScoreEvaluator.java
@@ -1,21 +1,28 @@
 package com.ssafy.backend.domain.util.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.ssafy.backend.domain.entity.Baekjoon;
 
 public class BojScoreEvaluator {
-	private final int SCALE = 10000;
-
-	public int scoreEvaluator(Baekjoon baekjoon) {
+	public static int scoreEvaluator(Baekjoon baekjoon) {
 		//제출문제 - (틀린문제 / 맞은문제) * 제출 - 시도했지만 맞은 문제 * (티어 / 맞은문제)
-
-		//스케일링 10000까지만 점수가 포함되게
-
-		return 0;
+		double score = Math.ceil(((double)baekjoon.getFailCount() / baekjoon.getPassCount()) * baekjoon.getSubmitCount()
+			- baekjoon.getTryFailCount() * (getTierNumberFromUrl(baekjoon.getTier()) / (double)baekjoon.getPassCount())
+			* 0.5);
+		return (int)score;
 	}
 
 	// 티어에서 숫자만 파싱하는 함수
-	public int parser(String tier) {
+	private static int getTierNumberFromUrl(String tier) {
+		Pattern pattern = Pattern.compile("tier/(\\d+)");
+		Matcher matcher = pattern.matcher(tier);
 
-		return 0;
+		if (matcher.find()) {
+			return Integer.parseInt(matcher.group(1));
+		} else {
+			return 0;
+		}
 	}
 }
