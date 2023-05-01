cb606baa6e03fab31dad9d06dccddf9c26edd147
정혜주 <fixup719@gmail.com>
2023-05-01 14:36:47 +0900

    Feat : redux persist 도입

--- frontend/redux/reducer.ts
+++ frontend/redux/reducer.ts
@@ -3,21 +3,24 @@ import { HYDRATE } from 'next-redux-wrapper';
 import countSlice from './countSlice';
 import splashSlice from './splashSlice';
 import authSlice from './authSlice';
-import storage from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
 
 const persistConfig = {
   key: 'root',
-  version: 1,
-  storage,
+  storage: storageSession,
 };
 
-const combinedReducer = combineReducers({
+const reducers = combineReducers({
   counter: countSlice,
   splashChecker: splashSlice,
   authChecker: authSlice,
 });
 
-const rootReducer: typeof combinedReducer = (state, action) => {
+const rootReducer = persistReducer(persistConfig, reducers);
+
+const persistedReducer: typeof rootReducer = (state, action) => {
   if (action.type === HYDRATE) {
     const nextState = {
       ...state,
@@ -25,7 +28,27 @@ const rootReducer: typeof combinedReducer = (state, action) => {
     };
     return nextState;
   } else {
-    return combinedReducer(state, action);
+    return rootReducer(state, action);
   }
 };
-export default rootReducer;
+
+export default persistedReducer;
+
+// const combinedReducer = combineReducers({
+//   counter: countSlice,
+//   splashChecker: splashSlice,
+//   authChecker: authSlice,
+// });
+
+// const rootReducer: typeof combinedReducer = (state, action) => {
+//   if (action.type === HYDRATE) {
+//     const nextState = {
+//       ...state,
+//       ...action.payload,
+//     };
+//     return nextState;
+//   } else {
+//     return combinedReducer(state, action);
+//   }
+// };
+// export default rootReducer;
