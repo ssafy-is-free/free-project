33e0ad39e4d328edc9379f447f8a889dea4bc65e
noobsoda <20151565@vision.hoseo.edu>
2023-05-02 17:53:04 +0900

    Fix : 백준 충돌 해결

--- backend/src/test/java/com/ssafy/backend/domain/algorithm/service/BojServiceImplTest.java
+++ backend/src/test/java/com/ssafy/backend/domain/algorithm/service/BojServiceImplTest.java
@@ -19,6 +19,7 @@ import com.ssafy.backend.domain.algorithm.repository.BojRepository;
 import com.ssafy.backend.domain.entity.Baekjoon;
 import com.ssafy.backend.domain.entity.User;
 import com.ssafy.backend.domain.job.repository.JobHistoryQueryRepository;
+import com.ssafy.backend.domain.job.repository.JobHistoryRepository;
 import com.ssafy.backend.domain.job.repository.JobPostingRepository;
 import com.ssafy.backend.domain.user.repository.UserQueryRepository;
 import com.ssafy.backend.domain.user.repository.UserRepository;
@@ -46,6 +47,8 @@ public class BojServiceImplTest {
 	@Mock
 	private JobPostingRepository jobPostingRepository;
 	@Mock
+	private JobHistoryRepository jobHistoryRepository;
+	@Mock
 	private JobHistoryQueryRepository jobHistoryQueryRepository;
 	private AlgorithmServiceImpl algorithmService;
 
@@ -53,7 +56,7 @@ public class BojServiceImplTest {
 	void setUp() {
 		this.algorithmService = new AlgorithmServiceImpl(bojRepository, bojLanguageRepository, languageRepository,
 			userRepository, userQueryRepository, bojQueryRepository, bojLanguageQueryRepository
-			, jobPostingRepository, jobHistoryQueryRepository);
+			, jobPostingRepository, jobHistoryRepository, jobHistoryQueryRepository);
 	}
 
 	@Test
