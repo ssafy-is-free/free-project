fdefd338ea2cd7f72cb2b8965c57eb790d7656d9
noobsoda <20151565@vision.hoseo.edu>
2023-05-04 15:11:00 +0900

    Feat : 백준 유저 전체 비교 기능 추가

--- backend/src/main/java/com/ssafy/backend/domain/algorithm/repository/BojLanguageQueryRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/algorithm/repository/BojLanguageQueryRepository.java
@@ -1,10 +1,14 @@
 package com.ssafy.backend.domain.algorithm.repository;
 
+import static com.ssafy.backend.domain.entity.QBaekjoonLanguage.*;
+
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.backend.domain.analysis.dto.BojLanguagePassCount;
+import com.ssafy.backend.domain.analysis.dto.QBojLanguagePassCount;
 import com.ssafy.backend.domain.entity.BaekjoonLanguage;
 import com.ssafy.backend.domain.entity.QBaekjoon;
 import com.ssafy.backend.domain.entity.QBaekjoonLanguage;
@@ -28,6 +32,17 @@ public class BojLanguageQueryRepository {
 			.selectFrom(baekjoonLanguage)
 			.leftJoin(baekjoonLanguage.baekjoon, baekjoon).fetchJoin()
 			.where(baekjoonLanguage.languageId.eq(language))
+			.fetch();
+	}
+
+	public List<BojLanguagePassCount> findBojLanguagePassCount() {
+		return queryFactory
+			.select(new QBojLanguagePassCount(baekjoonLanguage.languageId,
+				baekjoonLanguage.passCount.sum()))
+			.from(baekjoonLanguage)
+			.groupBy(baekjoonLanguage.languageId)
+			.orderBy(baekjoonLanguage.passCount.sum().desc())
+			.limit(5)
 			.fetch();
 	}
 }
--- backend/src/main/java/com/ssafy/backend/domain/analysis/service/AnalysisBojService.java
+++ backend/src/main/java/com/ssafy/backend/domain/analysis/service/AnalysisBojService.java
@@ -11,19 +11,29 @@ import org.springframework.stereotype.Service;
 
 import com.ssafy.backend.domain.algorithm.dto.response.BojInfoDetailResponse;
 import com.ssafy.backend.domain.algorithm.dto.response.BojLanguageResponse;
+import com.ssafy.backend.domain.algorithm.repository.BojLanguageQueryRepository;
 import com.ssafy.backend.domain.algorithm.repository.BojLanguageRepository;
 import com.ssafy.backend.domain.algorithm.repository.BojQueryRepository;
 import com.ssafy.backend.domain.algorithm.repository.BojRepository;
+import com.ssafy.backend.domain.analysis.dto.BojAvgDetail;
 import com.ssafy.backend.domain.analysis.dto.BojInfoAvgDetailResponse;
+import com.ssafy.backend.domain.analysis.dto.BojLanguagePassCount;
+import com.ssafy.backend.domain.analysis.dto.BojRankAllComparison;
 import com.ssafy.backend.domain.analysis.dto.BojRankComparison;
 import com.ssafy.backend.domain.entity.Baekjoon;
 import com.ssafy.backend.domain.entity.BaekjoonLanguage;
+import com.ssafy.backend.domain.entity.JobHistory;
+import com.ssafy.backend.domain.entity.JobPosting;
 import com.ssafy.backend.domain.entity.Language;
 import com.ssafy.backend.domain.entity.User;
 import com.ssafy.backend.domain.entity.common.LanguageType;
+import com.ssafy.backend.domain.github.dto.FilteredUserIdSet;
+import com.ssafy.backend.domain.job.repository.JobHistoryRepository;
+import com.ssafy.backend.domain.job.repository.JobPostingRepository;
 import com.ssafy.backend.domain.user.repository.UserRepository;
 import com.ssafy.backend.domain.util.repository.LanguageRepository;
 import com.ssafy.backend.global.response.exception.CustomException;
+import com.ssafy.backend.global.response.exception.CustomExceptionStatus;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -37,6 +47,9 @@ public class AnalysisBojService {
 	private final BojLanguageRepository bojLanguageRepository;
 	private final LanguageRepository languageRepository;
 	private final BojQueryRepository bojQueryRepository;
+	private final BojLanguageQueryRepository bojLanguageQueryRepository;
+	private final JobPostingRepository jobPostingRepository;
+	private final JobHistoryRepository jobHistoryRepository;
 
 	public BojRankComparison compareWithOpponent(long id, Long userId) {
 		//언어 정보를 저장할 해쉬맵
@@ -50,17 +63,31 @@ public class AnalysisBojService {
 
 	}
 
-	public BojRankComparison compareWithOther(long id, Long jobPostingId) {
+	public BojRankAllComparison compareWithOther(long id, Long jobPostingId) {
 		//언어 정보를 저장할 해쉬맵
 		Map<Long, String> languageMap = getLanguageMap();
 		//백준 디테일 정보 저장
 		BojInfoDetailResponse my = createBojInfoDetail(id, languageMap);
 
-		//백준 전체 정보 저장
-		BojInfoAvgDetailResponse other = null;
-		//passCount top 5 And passCount / 인원 수 보내기
+		//공고별로 필터링된 userIds
+		FilteredUserIdSet userIdSet = (jobPostingId == null) ? null : getUserIdByJobPosting(jobPostingId);
 
-		return null;
+		//공고별 전체 평균값 구하기
+		BojAvgDetail bojAvgDetail = bojQueryRepository.findByAvg(userIdSet);
+		//값 포맷팅
+		bojAvgDetail.roundToDecimalPlace();
+
+		//언어별 pass 최상위 5가지
+		List<BojLanguagePassCount> bojLanguagePassCount = bojLanguageQueryRepository.findBojLanguagePassCount();
+		List<BojLanguageResponse> bojLanguageList = bojLanguagePassCount.stream()
+			.map(u -> BojLanguageResponse.create(languageMap.get(u.getLanguageId()),
+				null, u.getTotalPassCount()))
+			.collect(Collectors.toList());
+
+		//백준 전체 정보 저장
+		BojInfoAvgDetailResponse other = BojInfoAvgDetailResponse.create(bojAvgDetail, bojLanguageList);
+
+		return BojRankAllComparison.builder().my(my).other(other).build();
 	}
 
 	private Map<Long, String> getLanguageMap() {
@@ -92,13 +119,12 @@ public class AnalysisBojService {
 		return BojInfoDetailResponse.create(user, baekjoon, bojLanguageList);
 	}
 
-	private List<BojLanguageResponse> createBojLanguageList(long bojId, HashMap<Long, String> languageMap) {
-		List<BaekjoonLanguage> bojList = bojLanguageRepository.findAllByBaekjoonId(bojId);
+	private FilteredUserIdSet getUserIdByJobPosting(Long jobPostingId) {
+		//공고 유효성 검증
+		JobPosting jobPosting = jobPostingRepository.findById(jobPostingId)
+			.orElseThrow(() -> new CustomException(CustomExceptionStatus.NOT_FOUND_JOBPOSTING));
 
-		return bojList.stream()
-			.map(u -> BojLanguageResponse.create(languageMap.get(u.getLanguageId()), u.getPassPercentage(),
-				u.getPassCount()))
-			.collect(Collectors.toList());
-
+		List<JobHistory> jobHistoryList = jobHistoryRepository.findByJobPosting(jobPosting);
+		return FilteredUserIdSet.create(jobHistoryList);
 	}
 }
