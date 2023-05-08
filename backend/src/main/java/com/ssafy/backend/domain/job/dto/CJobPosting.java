5a25353a2c7ce7db7a8476461457b21e23736a91
이성복 <seungbok3240@gmail.com>
2023-05-08 12:30:10 +0900

    Feat: job posting crawling to database

---
+++ backend/src/main/java/com/ssafy/backend/domain/job/dto/CJobPosting.java
@@ -0,0 +1,15 @@
+package com.ssafy.backend.domain.job.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CJobPosting {
+
+	private String companyName;
+
+	private String postingName;
+
+	private String start;
+
+	private String end;
+}
