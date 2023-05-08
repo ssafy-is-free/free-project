bc1d735d4eb2117ce5e513c32fb93bc0b5d855c9
yeonjin <y_jin99@naver.com>
2023-05-08 13:49:44 +0900

    Fix : 일반 조회쿼리 -> 카운트 쿼리

--- backend/src/main/java/com/ssafy/backend/domain/github/repository/GithubRepoRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/github/repository/GithubRepoRepository.java
@@ -4,9 +4,12 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ssafy.backend.domain.entity.Github;
 import com.ssafy.backend.domain.entity.GithubRepo;
 
 public interface GithubRepoRepository extends JpaRepository<GithubRepo, Long> {
 
 	long countByGithubIdIn(Set<Long> githubId);
+
+	long countByGithub(Github github);
 }
