2941782f09159e4b86acf763f677becbb12dd623
happyd918 <0happydeath0@naver.com>
2023-05-11 15:19:02 +0900

    Feat : device detect skeleton

---
+++ frontend/components/common/WebGuide.tsx
@@ -0,0 +1,10 @@
+const WebGuide = () => {
+  return (
+    <div>
+      <div>모바일로접속해주세요</div>
+      <div>F12를 눌러 Device Mode 를 모바일로 바꿔주세요</div>
+    </div>
+  );
+};
+
+export default WebGuide;
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import wrapper, { RootState, persistor } from '@/redux';
 import { Provider, useDispatch, useSelector } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { lightTheme } from '@/styles/theme';
 import GlobalStyle from '@/styles/GlobalStyle';
 import Footer from '@/components/common/Footer';
@@ -11,6 +11,12 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { useEffect } from 'react';
 import * as gtag from '@/utils/gtag';
 import Script from 'next/script';
+import { isBrowser } from 'react-device-detect';
+import WebGuide from '@/components/common/WebGuide';
+
+const SSS = styled.div`
+  width: 400px;
+`;
 
 function App({ Component, ...rest }: AppProps) {
   // google analytics
@@ -25,6 +31,10 @@ function App({ Component, ...rest }: AppProps) {
       setCookie('redirect-uri', 'k8b');
     }
   }, []);
+
+  if (isBrowser) {
+    return <WebGuide></WebGuide>;
+  }
 
   return (
     <>
