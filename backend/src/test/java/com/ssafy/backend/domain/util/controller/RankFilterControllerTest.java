1688f27faf3a878158eb4544b02a60ca4f011f36
lsh80165 <lsh80165@gmail.com>
2023-05-12 14:08:57 +0900

    Test : 랭킹 필터 컨트롤러 테스트

--- backend/src/test/java/com/ssafy/backend/domain/util/controller/RankFilterControllerTest.java
+++ backend/src/test/java/com/ssafy/backend/domain/util/controller/RankFilterControllerTest.java
@@ -1,7 +1,9 @@
 package com.ssafy.backend.domain.util.controller;
 
+import static com.ssafy.backend.global.response.CustomSuccessStatus.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,30 +16,37 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.ssafy.backend.domain.EnableMockMvc;
 import com.ssafy.backend.domain.util.dto.LanguageResponse;
 import com.ssafy.backend.domain.util.service.NotificationManager;
 import com.ssafy.backend.domain.util.service.RankFilterService;
 import com.ssafy.backend.global.auth.filter.TokenAuthenticationFilter;
 import com.ssafy.backend.global.config.sercurity.SecurityConfig;
 import com.ssafy.backend.global.exception.ControllerAdvisor;
+import com.ssafy.backend.global.response.DataResponse;
 import com.ssafy.backend.global.response.ResponseService;
+import com.ssafy.backend.global.response.ResponseStatus;
 
-@WebMvcTest(controllers = RankFilterController.class
-	, excludeFilters = {@ComponentScan.Filter(
-	type = FilterType.ASSIGNABLE_TYPE,
-	classes = {SecurityConfig.class, TokenAuthenticationFilter.class, NotificationManager.class,
-		ControllerAdvisor.class}
-)
-})
+@WebMvcTest(controllers = RankFilterController.class,
+	excludeFilters = {@ComponentScan.Filter(
+		type = FilterType.ASSIGNABLE_TYPE,
+		classes = {SecurityConfig.class, TokenAuthenticationFilter.class,
+			NotificationManager.class,
+			ControllerAdvisor.class}
+	)
+	})
+@EnableMockMvc
+@WithMockUser(username = "test")
 @DisplayName("랭킹 필터 테스트")
 class RankFilterControllerTest {
 
 	@Autowired
-	MockMvc mvc;
+	private MockMvc mvc;
 
 	@MockBean
 	private ResponseService responseService;
@@ -46,7 +55,6 @@ class RankFilterControllerTest {
 	private RankFilterService rankFilterService;
 
 	@Test
-	// @WithMockUser(username = "test")
 	@DisplayName("언어 목록 조회 테스트 - 백준")
 	void rankFilterControllerBojTest() throws Exception {
 		//given
@@ -60,8 +68,17 @@ class RankFilterControllerTest {
 		given(rankFilterService.getLanguageList(type))
 			.willReturn(languageResponseList);
 
+		DataResponse<List<LanguageResponse>> dataResponse = new DataResponse<>();
+		dataResponse.setStatus(String.valueOf(ResponseStatus.SUCCESS));
+		dataResponse.setMessage("요청에 성공했습니다.");
+		dataResponse.setData(languageResponseList);
+
+		given(responseService.getDataResponse(languageResponseList, RESPONSE_SUCCESS))
+			.willReturn(dataResponse);
+
 		//when
-		ResultActions actions = mvc.perform(get("/filter/language?type=" + type)
+		ResultActions actions = mvc.perform(get("/filter/language")
+			.param("type", type)
 			.contentType(MediaType.APPLICATION_JSON)
 			.accept(MediaType.APPLICATION_JSON)
 			.characterEncoding("UTF-8"));
@@ -69,29 +86,46 @@ class RankFilterControllerTest {
 		//then
 		actions
 			.andExpect(MockMvcResultMatchers.status().isOk())
-			.andExpect(MockMvcResultMatchers.content().string("test"));
+			.andExpect(jsonPath("status", "SUCCESS").exists())
+			.andExpect(jsonPath("message", "요청에 성공했습니다.").exists())
+			.andExpect(jsonPath("data", languageResponseList).exists());
 
 	}
 
 	@Test
 	@DisplayName("언어 목록 조회 테스트 - 깃허브")
-	void rankFilterControllerGithubTest() {
+	void rankFilterControllerGithubTest() throws Exception {
 		//given
+		String type = "github";
+		List<LanguageResponse> languageResponseList = new ArrayList<>();
+		languageResponseList.add(createLanguageResponse(1L, "C++"));
+		languageResponseList.add(createLanguageResponse(2L, "JAVA"));
+		languageResponseList.add(createLanguageResponse(3L, "PYTHON3"));
+
+		given(rankFilterService.getLanguageList(type))
+			.willReturn(languageResponseList);
+
+		DataResponse<List<LanguageResponse>> dataResponse = new DataResponse<>();
+		dataResponse.setStatus(String.valueOf(ResponseStatus.SUCCESS));
+		dataResponse.setMessage("요청에 성공했습니다.");
+		dataResponse.setData(languageResponseList);
+
+		given(responseService.getDataResponse(languageResponseList, RESPONSE_SUCCESS))
+			.willReturn(dataResponse);
 
 		//when
+		ResultActions actions = mvc.perform(get("/filter/language")
+			.param("type", type)
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON)
+			.characterEncoding("UTF-8"));
 
 		//then
-
-	}
-
-	@Test
-	@DisplayName("언어 목록 조회 테스트 - 잘못된 파라미터")
-	void rankFilterControllerFailTest() {
-		//given
-
-		//when
-
-		//then
+		actions
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(jsonPath("status", "SUCCESS").exists())
+			.andExpect(jsonPath("message", "요청에 성공했습니다.").exists())
+			.andExpect(jsonPath("data", languageResponseList).exists());
 
 	}
 
