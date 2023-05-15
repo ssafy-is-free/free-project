03221e05fd0ad99222ef47b0ae3675143360f25a
happyd918 <0happydeath0@naver.com>
2023-05-15 09:20:21 +0900

    Feat : 다음 일정이 과거일때 경고창

--- frontend/components/career/ModalDday.tsx
+++ frontend/components/career/ModalDday.tsx
@@ -42,6 +42,7 @@ const ModalDiv = styled.div`
 const DdayModal = ({ close, result, defaultDate }: IDdayModalProps) => {
   const [ddayName, setDdayName] = useState<string>('');
   const [date, setDate] = useState<string>('');
+  const today = new Date();
 
   const updateDdayName = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -56,9 +57,15 @@ const DdayModal = ({ close, result, defaultDate }: IDdayModalProps) => {
   };
 
   const ok = () => {
+    const inputDate = new Date(date);
     if (ddayName.length === 0) {
       Swal.fire({
         text: '다음 일정 이름을 입력해주세요',
+        icon: 'info',
+      });
+    } else if (inputDate < today) {
+      Swal.fire({
+        text: '다음 일정이 오늘보다 이전이에요',
         icon: 'info',
       });
     } else {
@@ -70,22 +77,21 @@ const DdayModal = ({ close, result, defaultDate }: IDdayModalProps) => {
   return (
     <ModalDiv>
       <div className="darkBg" onClick={close}></div>
-        <div className="modalContent">
-          <div className="modalTitle">다음일정 변경하기</div>
-          <InputDiv>
-            <div>다음일정 (최대 10글자)</div>
-            <input
-              type="text"
-              className="input"
-              placeholder="ex) 코딩테스트, 1차 면접"
-              value={ddayName}
-              onChange={updateDdayName}
-            ></input>
-          </InputDiv>
-          <DatePicker updateDate={updateDate} defaultDate={defaultDate}></DatePicker>
-          <CancelOk cancel={close} ok={ok}></CancelOk>
-        </div>
-      
+      <div className="modalContent">
+        <div className="modalTitle">다음일정 변경하기</div>
+        <InputDiv>
+          <div>다음일정 (최대 10글자)</div>
+          <input
+            type="text"
+            className="input"
+            placeholder="ex) 코딩테스트, 1차 면접"
+            value={ddayName}
+            onChange={updateDdayName}
+          ></input>
+        </InputDiv>
+        <DatePicker updateDate={updateDate} defaultDate={defaultDate}></DatePicker>
+        <CancelOk cancel={close} ok={ok}></CancelOk>
+      </div>
     </ModalDiv>
   );
 };
