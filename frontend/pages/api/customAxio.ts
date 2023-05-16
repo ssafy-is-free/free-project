1deb38395a494f07d536cf0af91c716bd99b5f96
정혜주 <fixup719@gmail.com>
2023-05-16 12:28:01 +0900

    Fix : console 제거, alert 수정

--- frontend/pages/api/customAxio.ts
+++ frontend/pages/api/customAxio.ts
@@ -62,10 +62,10 @@ authApi.interceptors.response.use(
             // return axios(originalRequest);
           } else if (res.data.status === 'FAIL') {
             //로그아웃 시키기
-            Swal.fire({
-              text: '로그아웃 되었습니다.',
-              icon: 'error',
-            });
+            // Swal.fire({
+            //   text: '로그아웃 되었습니다.',
+            //   icon: 'error',
+            // });
             localStorage.removeItem('accessToken');
             window.location.href = '/';
           }
@@ -73,10 +73,10 @@ authApi.interceptors.response.use(
         .catch((err) => {
           if (err.response.status === 401) {
             localStorage.removeItem('accessToken');
-            Swal.fire({
-              text: '로그아웃 되었습니다.',
-              icon: 'error',
-            });
+            // Swal.fire({
+            //   text: '로그아웃 되었습니다.',
+            //   icon: 'error',
+            // });
             window.location.href = '/';
           }
         });
