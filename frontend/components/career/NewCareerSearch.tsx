4bffa03316561ca4c96c5b1fa2485d1cf56ef897
happyd918 <0happydeath0@naver.com>
2023-05-16 14:23:29 +0900

    Feat : 검색 결과 수정

--- frontend/components/career/NewCareerSearch.tsx
+++ frontend/components/career/NewCareerSearch.tsx
@@ -75,6 +75,9 @@ const CareerSearch = ({ close, result }: ICareerSearchProps) => {
     if (value) {
       const res = await getJobPost(value);
       setData(res.data);
+    } else {
+      const res = await getJobPost(' ');
+      setData(res.data);
     }
   };
   return (
@@ -98,7 +101,10 @@ const CareerSearch = ({ close, result }: ICareerSearchProps) => {
             value={word}
             placeholder={'search...'}
             autoComplete="off"
-            onFocus={() => setNotFocus(false)}
+            onFocus={() => {
+              searching(' ');
+              setNotFocus(false);
+            }}
             onChange={(e) => {
               setWord(e.target.value);
               searching(e.target.value);
