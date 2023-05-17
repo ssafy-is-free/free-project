852cde31c4a3d8f9d4b459b25dea3e3a70d414cd
noobsoda <20151565@vision.hoseo.edu>
2023-05-17 13:41:30 +0900

    Test : 깃허브 서비스 테스트 코드 수정

--- backend/src/test/java/com/ssafy/backend/domain/github/service/GithubServiceTest.java
+++ backend/src/test/java/com/ssafy/backend/domain/github/service/GithubServiceTest.java
@@ -1,6 +1,10 @@
 package com.ssafy.backend.domain.github.service;
 
+import static com.ssafy.backend.global.response.exception.CustomExceptionStatus.*;
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.Arrays;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -11,14 +15,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ssafy.backend.domain.entity.Github;
 import com.ssafy.backend.domain.entity.GithubLanguage;
+import com.ssafy.backend.domain.entity.GithubRepo;
 import com.ssafy.backend.domain.entity.Language;
 import com.ssafy.backend.domain.entity.User;
 import com.ssafy.backend.domain.entity.common.LanguageType;
 import com.ssafy.backend.domain.github.dto.GithubDetailResponse;
+import com.ssafy.backend.domain.github.dto.OpenRequest;
+import com.ssafy.backend.domain.github.dto.ReadmeResponse;
 import com.ssafy.backend.domain.github.repository.GithubLanguageRepository;
+import com.ssafy.backend.domain.github.repository.GithubRepoRepository;
 import com.ssafy.backend.domain.github.repository.GithubRepository;
+import com.ssafy.backend.domain.user.dto.NicknameListResponse;
 import com.ssafy.backend.domain.user.repository.UserRepository;
 import com.ssafy.backend.domain.util.repository.LanguageRepository;
+import com.ssafy.backend.global.response.exception.CustomException;
 
 @SpringBootTest
 class GithubServiceTest {
@@ -30,6 +40,8 @@ class GithubServiceTest {
 	@Autowired
 	private GithubRepository githubRepository;
 	@Autowired
+	private GithubRepoRepository githubRepoRepository;
+	@Autowired
 	private GithubLanguageRepository githubLanguageRepository;
 	@Autowired
 	private LanguageRepository languageRepository;
@@ -38,6 +50,7 @@ class GithubServiceTest {
 	void tearDown() {
 		githubLanguageRepository.deleteAllInBatch();
 		languageRepository.deleteAllInBatch();
+		githubRepoRepository.deleteAllInBatch();
 		githubRepository.deleteAllInBatch();
 		userRepository.deleteAllInBatch();
 
@@ -135,8 +148,121 @@ class GithubServiceTest {
 		Assertions.assertThat(results.getRepositories()).isNull();
 	}
 
+	@Test
+	@DisplayName("깃허브 닉네임 리스트를 불러온다")
+	void getNicknameListTest() {
+		//given
+		User user1 = createUser("nickname1");
+		User user2 = createUser("nickname2");
+		User user3 = createUser("nickname3");
+
+		userRepository.saveAll(Arrays.asList(user1, user2, user3));
+
+		//when
+		List<NicknameListResponse> nicknameListResponses = githubService.getNicknameList("ckna");
+
+		//then
+		assertThat(nicknameListResponses).extracting(NicknameListResponse::getNickname)
+			.containsExactly("nickname1", "nickname2", "nickname3");
+	}
+
+	@Test
+	@DisplayName("깃허브 디테일 삭제된 유저의 경우")
+	void getDetailsDeleteUserTest() {
+		//given
+		User user1 = createUser("user1", true);
+		User user2 = createUser("user2");
+
+		userRepository.saveAll(Arrays.asList(user1, user2));
+
+		//when & then
+		assertThatThrownBy(() -> githubService.getDetails(user1.getId(), true)).isInstanceOf(CustomException.class)
+			.hasFieldOrPropertyWithValue("customExceptionStatus", IS_DELETED_USER);
+	}
+
+	@Test
+	@DisplayName("깃허브 리드미 불러오기 테스트")
+	void getReadmeTest() {
+		//given
+		User user = createUser("user");
+		Github github = createGithub(user, true);
+		GithubRepo githubRepo = createGithubRepo(github, "리드미");
+
+		userRepository.save(user);
+		githubRepository.save(github);
+		githubRepoRepository.save(githubRepo);
+		//when
+		ReadmeResponse readmeResponse = githubService.getReadme(github.getId(), githubRepo.getId());
+
+		//then
+		assertThat(readmeResponse).extracting(ReadmeResponse::getReadme).isEqualTo("리드미");
+
+	}
+
+	@Test
+	@DisplayName("깃허브 리드미 레포 Null 테스트")
+	void getReadmeRepoNullTest() {
+		//given
+		User user = createUser("user");
+		Github github = createGithub(user, true);
+		GithubRepo githubRepo = createGithubRepo(github, "리드미");
+
+		userRepository.save(user);
+		githubRepository.save(github);
+		githubRepoRepository.save(githubRepo);
+		// when & then
+		assertThatThrownBy(() -> githubService.getReadme(github.getId(), -1)).isInstanceOf(CustomException.class)
+			.hasFieldOrPropertyWithValue("customExceptionStatus", NOT_FOUND_REPOSITORY);
+
+	}
+
+	@Test
+	@DisplayName("비공개 처리 테스트")
+	void updatePublicTest() {
+		//given
+		User user = createUser("user");
+		Github github = createGithub(user, true);
+
+		userRepository.save(user);
+		githubRepository.save(github);
+
+		OpenRequest openRequest = new OpenRequest();
+		openRequest.setGithubId(github.getId());
+		openRequest.setOpenStatus(false);
+		//when
+		githubService.updatePublic(user.getId(), openRequest);
+		Github response = githubRepository.findById(github.getId()).get();
+		//then
+		assertThat(response.isPublic()).isFalse();
+	}
+
+	@Test
+	@DisplayName("비공개 처리 유저 Null 테스트")
+	void updatePublicUserNullTest() {
+		//given	//when	//then
+		assertThatThrownBy(() -> githubService.updatePublic(-1, new OpenRequest())).isInstanceOf(
+				CustomException.class)
+			.hasFieldOrPropertyWithValue("customExceptionStatus", NOT_FOUND_USER);
+	}
+
+	@Test
+	@DisplayName("비공개 처리 깃허브 Null 테스트")
+	void updatePublicGithubNullTest() {
+		//given
+		User user = createUser("user");
+		userRepository.save(user);
+		// when	//then
+		assertThatThrownBy(() -> githubService.updatePublic(user.getId(), new OpenRequest())).isInstanceOf(
+				CustomException.class)
+			.hasFieldOrPropertyWithValue("customExceptionStatus", NOT_FOUND_GITHUB);
+	}
+
 	private User createUser(String nickname) {
 		return User.builder().nickname(nickname).image("1").isDeleted(false).build();
+	}
+
+	private User createUser(String nickname, boolean isDeleted) {
+		return User.builder().nickname(nickname).image("1").isDeleted(isDeleted).build();
 	}
 
 	private Github createGithub(User user, boolean isPublic) {
@@ -149,6 +275,15 @@ class GithubServiceTest {
 			.profileLink("1")
 			.previousRank(1)
 			.isPublic(isPublic)
+			.build();
+	}
+
+	private GithubRepo createGithubRepo(Github github, String readme) {
+		return GithubRepo.builder()
+			.github(github)
+			.name("레포")
+			.readme(readme)
+			.repositoryLink("link")
 			.build();
 	}
 
