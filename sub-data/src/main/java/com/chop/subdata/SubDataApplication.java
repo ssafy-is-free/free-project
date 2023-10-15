07eea9d72d0fad9c52733d29bc02e149cecab888
seungboklee <seungbok.lee@villanema.com>
2023-10-15 16:53:13 +0900

    init project

---
+++ sub-data/src/main/java/com/chop/subdata/SubDataApplication.java
@@ -0,0 +1,13 @@
+package com.chop.subdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubDataApplication.class, args);
+	}
+
+}
---
+++ sub-data/src/test/java/com/chop/subdata/SubDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chop.subdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
