9d7ada593cbaa9fbd3212d0eb5e49e4cbf02aad6
seungboklee <seungbok.lee@villanema.com>
2023-10-29 20:56:06 +0900

    feat: base webClient module

--- sub-data/src/main/java/com/chop/subdata/client/GithubWebClient.java
+++ sub-data/src/main/java/com/chop/subdata/client/GithubWebClient.java
@@ -2,18 +2,25 @@ package com.chop.subdata.client;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
-@RequiredArgsConstructor
 public class GithubWebClient {
+  private static final String GITHUB_GRAPHQL = "https://api.github.com/graphql";
+  private final WebClient webClient;
 
-  private final WebClient githubWebClient;
+  public GithubWebClient() {
+    webClient = WebClient.builder()
+            .baseUrl(GITHUB_GRAPHQL)
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+  }
 
   public JsonNode getData(String accessToken, GraphQLRequest request) {
-    return githubWebClient.post()
+    return webClient.post()
             .header("Authorization", "Bearer " + accessToken)
             .bodyValue(request.getRequestBody())
             .retrieve()
