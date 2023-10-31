159556081ffa1435ab36b4c687824fa0a0ca2e98
lsh9672 <lsh80165@gmail.com>
2023-11-01 00:28:30 +0900

    Feat : 레디스 라이브러리 동작하지 않도록 exclude에 추가

--- backend/src/main/java/com/ssafy/backend/BackendApplication.java
+++ backend/src/main/java/com/ssafy/backend/BackendApplication.java
@@ -2,8 +2,9 @@ package com.ssafy.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication( exclude = {RedisAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
